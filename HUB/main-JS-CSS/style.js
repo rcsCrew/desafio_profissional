b290910fb75fbed9dbc6df86169203560f1a9acc
rcsCrew <ramonsenger282@outlook.com>
2022-05-17 01:24:51 -0300

    hamburg in 800px hide navbar

--- HUB/main-JS-CSS/style.js
+++ HUB/main-JS-CSS/style.js
@@ -5,11 +5,12 @@
 // frun.classList.toggle("Active", this.window.scrollY > 0);                 --->> cria ou muda a classe do frun para Active
 
 window.addEventListener("scroll", function(){
-    var frun = document.querySelector("frun");
-    frun.classList.toggle("Active", this.window.scrollY > 0);
+    var header = document.querySelector("header");
+    header.classList.toggle("Active", this.window.scrollY > 0);
 });
 
 window.addEventListener("scroll", function(){
     var div = document.querySelector("div");
     div.classList.toggle("Active1", this.window.scrollY > 0);
 })
+
